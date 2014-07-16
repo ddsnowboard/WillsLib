eb43a7365c5b030c3dc1bcfd71ea373c3ef0ea52
ddsnowboard <ddsnowboard@gmail.com>
2014-07-16 16:51:29 -0500

    Initial commit
    Started on function library with insert. Needs a lot of work.

---
+++ WillsLib.py
@@ -0,0 +1,9 @@
+import sqlite3
+import tkinter as tk
+from types import methodType
+# Put main DB interface functions from tshirtPicker in here. Generalize them, and they'll be useful. 
+def insert(DB_name, id, description, lasttime):
+	# I need to figure out how to get a variable amount of arguments and then put them into this line below, in the parens. 
+	# Yay. 
+	cursor.execute("insert into ? VALUES (?, ?, ?);", (id, description, lasttime))
+	db.commit()
