422ab917fa69a334fe5bae1e1b1b06f39647bb00
ddsnowboard <ddsnowboard@gmail.com>
2014-07-16 16:52:46 -0500

    Added comment

--- WillsLib.py
+++ WillsLib.py
@@ -2,6 +2,7 @@ import sqlite3
 import tkinter as tk
 from types import methodType
 # Put main DB interface functions from tshirtPicker in here. Generalize them, and they'll be useful. 
+# Also, use this: http://www.ianlewis.org/en/dynamically-adding-method-classes-or-class-instanc
 def insert(DB_name, id, description, lasttime):
 	# I need to figure out how to get a variable amount of arguments and then put them into this line below, in the parens. 
 	# Yay. 
