909939ed58135b1abec9def524279d5551813479
ddsnowboard <ddsnowboard@gmail.com>
2014-07-19 11:51:04 -0500

    Added DBupdate function.
    Need to update docs, because the implementation is a little complicated.
    But I tested it with more than one value and everything, I belive it works.

--- WillsLib.py
+++ WillsLib.py
@@ -5,6 +5,7 @@ import tkinter as tk
 # one and a DBselect to get all of them. 
 # Also, it would be cool to have a table object that is really python-y instead of
 # all annoying and sql-y. 
+# In 2.4, there is iteritems(), in 3 there is just items(). *sigh*
 def DBinsert(connection, table_name, vals):
 	s = 'insert into '+table_name+' VALUES (?'
 	for i in range(len(vals)-1):
@@ -30,3 +31,31 @@ def DBcreate(connection, table_name, columns):
 		s+=i
 	s+=');'
 	connection.cursor().execute(s)
+	connection.commit()
+def DBupdate(connection, table_name, set, which):
+	# Set and which will be dictionaries that have the syntax {column: value}\\
+	# Which could also be the string "all"
+	if not set:
+		raise Exception("""You didn't give the right parameters!\nYou need 
+						 to give 2 dictionaries, \"set\" and \"which\", that\n
+						 are in the format {attribute:value}. See the \n
+						 documentation for more details.""")
+	elif not which:
+		raise Exception("""You didn't give the right parameters!\nYou need 
+						 to give 2 dictionaries, \"set\" and \"which\", that\n
+						 are in the format {attribute:value}. See the \n
+						 documentation for more details.\n
+						 If you want to select all and change, use\n
+						 which='all'""")
+	strings = []
+	for i in sorted(set.keys()):
+		strings.append(str(i)+' = ?')
+	if which == 'all':
+		connection.cursor().execute("update "+table_name+" SET "+', '.join(strings),tuple([j for j in sorted(set.values())]))
+		connection.commit()
+	else:
+		params = []
+		for i in sorted(which.keys()):
+			params.append(str(i)+' = ?')
+		connection.cursor().execute("update "+table_name+" SET "+', '.join(strings)+" WHERE "+' and '.join(params),tuple([j for j in sorted(set.values())]+[i for i in sorted(which.values())]))
+	connection.commit()
--- tester.py
+++ tester.py
@@ -2,6 +2,8 @@ from WillsLib import *
 import sqlite3
 db = sqlite3.connect('test.db')
 c = db.cursor()
-DBinsert(db, 'test', ['alpha','beta', 'gamma', 'delta'])
+# DBinsert(db, 'test', ['Ames','Bink', 'Gruel', 'Dapper'])
 print(DBselect(db, 'test', 'all'))
-DBcreate(db, 'test1', ['red', 'green','blue'])
+DBupdate(db, 'test', {'b':'boring'}, {'c':'Gruel'})
+print("\n\n\n")
+print(DBselect(db, 'test', 'all'))
