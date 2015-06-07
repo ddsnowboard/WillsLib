251efe040789620ae076330abbbf999b54442659
ddsnowboard <ddsnowboard@gmail.com>
2015-06-07 16:18:36 -0500

    Add updated Equation object from NewtonsMethod.
    Also update README.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 WillsLib
 ========
 
-My library of python functions that I use often/like to have around, beginning with a set of mysqlite3 helper functions. 
+My library of python functions that I use often/like to have around, beginning with a set of mysqlite3 helper functions.
 
 
 ######WillsLib.DBinsert(connection, table_name, vals)
@@ -10,30 +10,30 @@ Runs a sqlite3 "insert" command with the connection on the table name given. Thi
 
 
 ######WillsLib.DBselect(connection, table_name, columns, which)
-	
+
 Runs a sqlite3 "select" command with the given connection on the given `table_name`, selecting the given columns and returning an array of tuples containing the selected information. If you give `columns` as a string `'all'`, it will give you all the columns. If you want more than one column, give it as an array of strings. If you want only one, give it as an array of strings or a string. `which` is a dictionary in the format `{attribute:value}`, just like the others, or `'all'` to get all of the rows. If you give it a dictionary, it will only give you the rows whose values match the one(s) specified in the dictionary.  
 
 
 ######WillsLib.DBcreate(connection, table_name, columns)
 
-Runs a sqlite3 "create" command with the given connection to create the given named table with the given columns. Columns are in an array of strings. 
+Runs a sqlite3 "create" command with the given connection to create the given named table with the given columns. Columns are in an array of strings.
 
 
 ######WillsLib.DBupdate(connection, table_name, set, which)
 
 Updates the given attributes to the new ones in the "set" variable. Set and which are dictionaries, and they are in the format `{column:attribute}`. Set is what you want to change, and which is where you want to change it. If you wanted to change Zachary's address, you'd do this:
-	
+
     DBupdate(connection, "table_name", {'address':'7278 Main St.'}, {'Name':'Zachary'})
 
 
-You can also set all the table elements to have a certain attribute by passing 'all' to which instead of a dictionary. 
-	
+You can also set all the table elements to have a certain attribute by passing 'all' to which instead of a dictionary.
+
     DBupdate(connection, "hitlist", {"status":"dead"}, "all")
 
 
 ######WillsLib.DBdelete(connection, table_name, which)
 
-Deletes the entries dictated by `which` in the given table, `table_name`. `which` is a dictionary in the format `{attribute:value}`, just like in DBupdate(). 
+Deletes the entries dictated by `which` in the given table, `table_name`. `which` is a dictionary in the format `{attribute:value}`, just like in DBupdate().
 
 ######WillsLib.sanitize(string)
 
@@ -41,56 +41,66 @@ This is an attempt at sanitizing database inputs. If this were Java, it would pr
 
 ######WillsLib.primes()
 
-This will return a prime number generator, using the brute force method, with a slight optimization. 
+This will return a prime number generator, using the brute force method, with a slight optimization.
 
 
 ######WillsLib.squares()
 
-This returns a perfect square generator that is actually pretty fast. 
+This returns a perfect square generator that is actually pretty fast.
 
 ######*class* WillsLib.Equation(equation)
 
-This returns an instance of the Equation object, made with the given equation in string form. It must have an equal sign. You can use either `^` or `**` for exponentiation. You can use any letter for the variable, but you must be consistent. 
+This returns an instance of the Equation object, made with the given equation in string form. It must have an equal sign. You can use either `^` or `**` for exponentiation. You can use any letter for the variable, but you must be consistent.
 
 ######Equation methods:
 
 + Equation.evaluate(x)
 
-  + This evaluates the equation for the given input. 
+  + This evaluates the equation for the given input.
 
 + Equation.intersect(other)
 
-  + Given another Equation, this finds the intersection of them if possible. It's not very robust, but it works sometimes. 
+  + Given another Equation, this finds the intersection of them if possible. It's not very robust, but it works sometimes.
 
 ######WillsLib.myIndex(list, value, func = lambda x: x)
 
-This is my own (probably slower) implementation of python's list.index() function. This allows you to set what aspect of each item you 
-want to check, similar to the default list.sort() function. For example, if I want to find the first thing in a list that has a length 
+This is my own (probably slower) implementation of python's list.index() function. This allows you to set what aspect of each item you
+want to check, similar to the default list.sort() function. For example, if I want to find the first thing in a list that has a length
 of 5, I could say
 
 ```
 WillsLib.myIndex(l, 5, len)
 ```
 
-and it would return the index of that in the list. 
+and it would return the index of that in the list.
 
 
 ######WillsLib.tabsToList(input_list, output_filename, type = "ordered")
 
-This takes a text file (or just a list of lines) that is indented with tabs and turns it into an HTML file that is indented with the given type (ordered or unordered) of HTML list. The formatting of the HTML isn't too pretty, so you might want to run it through a formatter, but it does work. 
+This takes a text file (or just a list of lines) that is indented with tabs and turns it into an HTML file that is indented with the given type (ordered or unordered) of HTML list. The formatting of the HTML isn't too pretty, so you might want to run it through a formatter, but it does work.
 
 ######WillsLib.myRange(start, stop, step)
 
-A version of range/xrange that returns a generator that goes from `start` (inclusive) to `stop` (exclusive) by `step` each time. It is not as fast as range/xrange, but it supports floats. 
+A version of range/xrange that returns a generator that goes from `start` (inclusive) to `stop` (exclusive) by `step` each time. It is not as fast as range/xrange, but it supports floats.
 
 ######WillsLib.euler(function, x, x0, y0, h)
 
-A function that solves certain differential equations by using Euler's method. `function` is a lambda or regular function that takes two arguments, which will be `x` and `y`. `x0` and `y0` are the initial conditions, and `h` is Δx or the step-size. 
+A function that solves certain differential equations by using Euler's method. `function` is a lambda or regular function that takes two arguments, which will be `x` and `y`. `x0` and `y0` are the initial conditions, and `h` is Δx or the step-size.
 
 ######WillsLib.eulerTable(function, x, x0, y0, h, mainloop)
 
-Similar to euler(), but creates a table in a tkinter window which lists every step. `mainloop` is a boolean that tells whether `mainloop()` should be run at the end. 
+Similar to euler(), but creates a table in a tkinter window which lists every step. `mainloop` is a boolean that tells whether `mainloop()` should be run at the end.
 
 ######WillsLib.factors(integer)
 
-Returns a list of all the factors of a number except itself and 1, or an empty list if it is prime. It runs in n/2 time always, so it is rather slow, but it works. 
+Returns a list of all the factors of a number except itself and 1, or an empty list if it is prime. It runs in n/2 time always, so it is rather slow, but it works.
+
+#####*class* WillsLib.PrimeFactorizer()
+
+This is a class that creates an object that more efficiently finds the prime factorization of numbers than a function could alone because it caches the prime numbers between runs.
+
+#####PrimeFactorizer methods
+
++ PrimeFactorizer.factorize(number)
+
+  + This factorizes a number, returning a list of the prime factors. 
--- WillsLib.py
+++ WillsLib.py
@@ -109,39 +109,122 @@ def primes():
 		out+=2
 # This is a class for polynomial equations. Give it the equation as a string, and you can evaluate it and maybe find the intersection
 # with another line. 
+def roundUp(i):
+	"""
+	I'm sure this is unnecessary, but I apparently couldn't find a good way to
+	do it in the standard library, so I have this.
+	"""
+	if i % 1 > 0:
+		return int(i)+1
+	return i
+class ZeroError(Exception):
+	pass
 class Equation:
+	"""
+	This is an object that represents a polynomial equation.
+	It isn't terribly extensible, so you have to be careful what
+	you give it. It will take decimals and fractions as coefficients,
+	and in exponents.
+	"""
 	def __init__(self, eq):	# y=2x^2-3x+5
-		self.coefficients = defaultdict(float)
-		self.eq = re.subn(r"^y=|=y$", '', eq)[0]   # 2x^2-3x+5
-		self.eq = self.eq.replace('^', '**').replace("+", " +").replace("-", ' -')  # 2x**2 -3x +5
-		self.terms = self.eq.split(" ")	 # "2x**2", "-3x", "+5"
-		self.terms = [i for i in self.terms if i != '']
-		for i in self.terms:
-			if not re.compile(r"[A-Za-z]").search(i):
-				self.coefficients[0] += float(i)  # "+5"
-			elif re.compile(r"[\+-]?[\d\.]+[A-Za-z]$").search(i):
-				self.coefficients[1]+=float(re.compile(r"[A-Za-z]").subn('',i)[0])  	#"-3" 
-			elif re.compile(r"[\+-]?[\d\.]+[A-Za-z]\*\*\d+").match(i):
-				self.coefficients[i[i.index("**")+2:]] += float(i[:re.compile("[A-Za-z]").search(i).span()[1]-1]) # '2'
-		self.degree = len(self.coefficients)-1
+	# If the equation is already a string, we have to turn it into an Equation
+	# object.
+		if type(eq) == type(""):
+			eq = eq.replace(" ", "")
+			# "normal" is an exponent greater than 1. "first" is an exponent of
+			# 1.
+			self.regexes = {"normal"   : re.compile(r"(?P<number>[\+-]?([\d\./])*)?[A-Za-z][\^](?P<exponent>[0-9/.]+)"),
+							"constant" : re.compile(r"^[\+-]?[\d/^]+$"),
+							"first"    : re.compile(r"(?P<number>[\+-]?[\d\./]*)?[A-Za-z]")}
+
+			self.coefficients = defaultdict(float)
+			self.eq = re.subn(r"^y=|=y$", '', eq)[0]   # 2x^2-3x+5
+			# Add in spaces for later.
+			self.eq = self.eq.replace("**", "^").replace("+", " +").replace("-", ' -')  # 2x^2 -3x +5
+			self.terms = self.eq.split(" ")	 # "2x^2", "-3x", "+5"
+			self.terms = [i for i in self.terms if i != '']
+			for i in self.terms:
+				if self.regexes['constant'].match(i):
+					self.coefficients[0] += eval(i.replace("^", "**"))  # "+5"
+				elif self.regexes['normal'].match(i):
+					match = self.regexes['normal'].match(i)
+					# If there is a number part on the term and it isn't just a
+					# plus sign (either a minus sign or an actual number)
+					if match.group("number") and match.group("number") != '+':
+						if match.group("number") == "-":
+							self.coefficients[Fraction(match.group("exponent"))] -= 1
+						else:
+							self.coefficients[Fraction(match.group("exponent"))] += Fraction(match.group("number"))
+					else:
+						# If the number is just a plus sign, make it one.
+						self.coefficients[Fraction(match.group("exponent"))] += 1
+				elif self.regexes["first"].match(i):
+					match = self.regexes["first"].match(i)
+					if match.group("number") and match.group("number") != "+":
+						if match.group("number") == '-':
+							self.coefficients[1] -= 1
+						else:
+							self.coefficients[1] += Fraction(match.group('number'))  	#"-3"
+					else:
+						self.coefficients[1] += 1
+		elif type(eq) == type({}):
+			# If our input is a dictionary, make that dictionary the coefficients
+			# dictionary and be done.
+			self.coefficients = defaultdict(float)
+			for i, j in eq.items():
+				self.coefficients[i] = j
+		self.degree = max(self.coefficients.keys())
 	def evaluate(self, x):
+		"""
+		This function simply evaluates the Equation. You cal also use
+		the __call__ function (eqn(x)).
+		It goes through the coefficients dictionary and adds up all the
+		terms.
+		"""
 		end = 0
 		for i, j in self.coefficients.items():
-			end+=j*x**i
+			try:
+				end+=j*x**i
+			except ZeroDivisionError:
+				raise Exception("I had to divide by zero.")
 		return end
+	def zero(self):
+		"""
+		This will find the zero of a quadratic function using the quadratic
+		equation.
+		I could implement a way to find it numerically for other
+		types, but maybe another time.
+		"""
+		if not self.isQuadratic():
+			raise ZeroError("This function isn't quadratic")
+		a = self.coefficients[2]
+		b = self.coefficients[1]
+		c = self.coefficients[0]
+		try:
+			return tuple(sorted(((-1 * b + math.sqrt(b**2 - 4 * a * c)) / (2 * a), (-1 * b - math.sqrt(b**2 - 4 * a * c)) / (2 * a))))
+		except ValueError:
+			return None
 	def intersect(self, other):
-		if not type(other) == type(Equation("2x^2-4x+5")):
-			raise Exception("You seem to have made a stupid; this is supposed to take another equation and find the intersection")
+		"""
+		This function finds the intersection of two equations.
+		It returns a tuple of the form (x, y), or a boolean if the functions are
+		the same or will never intersect.
+		"""
+		if not type(other) == type(Equation("5")):
+			raise Exception("Arguments {}, {} don't match Equation, Equation".format(type(self), type(other)))
 			return
-		# Left will be variables; right will be constants. 
-		# Left starts as self, right starts as other. 
+		# Left will be variables; right will be constants.
+		# Left starts as self, right starts as other.
 		left = defaultdict(float)
 		right = 0
+		# Go through the left hand function (self) and move all the constants to
+		# the right and leave the variables on the left.
 		for i, j in self.coefficients.items():
 			if i == 0:
 				right-=j
 			else:
 				left[i]+=j
+		# Then do the same on the right.
 		for i, j in other.coefficients.items():
 			if i == 0:
 				right+=j
@@ -149,13 +232,58 @@ class Equation:
 				left[i]-=j
 		if self.degree == 0 and other.degree == 0:
 			return right == 0
-		elif self.degree<=1 and other.degree<=1:
+		elif self.degree <= 1 and other.degree <= 1 :
 			return (right/left[1], self.evaluate(right/left[1]))
+		# Runs the quadratic equation if a degree is two. 
 		elif self.degree == 2 or other.degree == 2:
-			# Returns quadratic formula
 			return (((-1*left[1]+math.sqrt(left[1]**2-4*(left[2])*(-1*right)))/(2*left[2]), self.evaluate((-1*left[1]+math.sqrt(left[1]**2-4*(left[2])*(-1*right)))/(2*left[2]))), ((-1*left[1]-math.sqrt(left[1]**2-4*(left[2])*(-1*right)))/(2*left[2]), self.evaluate((-1*left[1]-math.sqrt(left[1]**2-4*(left[2])*(-1*right)))/(2*left[2]))))
 		else:
 			raise Error("I really can't get an accurate intersection with just this data.")
+	def __str__(self):
+		out = ""
+		sortedDict = OrderedDict(sorted(self.coefficients.items(), key=lambda x: -1*abs(x[0])))
+		for degree, number in sortedDict.items():
+			if number == 0:
+				continue
+			elif degree == 0:
+				out+= ('+' if number > 0 else "") + str(number)
+			elif number == 1 and degree == 1:
+				out+= '+x'
+			elif number == -1 and degree == 1:
+				out += '-x'
+			elif degree == 1:
+				out += "{}{}x".format(('+' if number > 0 and degree != self.degree else ''), number)
+			else:
+				out+="{}{}x^{}".format(('+' if number > 0 and degree != self.degree else ""),str(number),str(degree))
+		return out
+	def derivative(self):
+		new = {}
+		for i, j in self.coefficients.items():
+			new[i-1] = j*i if i != 0 else 0
+		return Equation(new)
+	def isQuadratic(self):
+		POSSIBLE_DEGREES = [0, 1, 2]
+		if self.degree != 2:
+			return False
+		for i in self.coefficients.keys():
+			if not i in POSSIBLE_DEGREES:
+				return False
+		return True
+	def __eq__(self, other):
+		if self.degree == other.degree:
+			if self.coefficients == other.coefficients:
+				return True
+		return False
+	def __ne__(self, other):
+		return not self.__eq__(self, other)
+	def __bool__(self, other):
+		return self == Equation("0")
+	def __getitem__(self, key):
+		return self.coefficients[key]
+	def __setitem__(self, key, value):
+		self.coefficients[key] = value
+	def __call__(self, x):
+		return self.evaluate(x)
 def myIndex(l, value, func = lambda x: x):
 	for i, j in enumerate(l):
 		if func(j) == value:
@@ -248,9 +376,9 @@ class PrimeFactorizer:
         while True:
             if current_number in self.primes:
                 out.append(current_number)
-                return out
+                return sorted(out)
             elif current_number == 1:
-                return out
+                return sorted(out)
             if len(self.primes) == counter and number not in self.primes:
                 self.primes.append(next(self.prime_generator))
             if current_number % self.primes[counter] == 0:
